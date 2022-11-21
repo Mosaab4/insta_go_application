f373796c772f4c217507d0cf3677e82a710e9299
Mosaab <musab.ehab@gmail.com>
2022-11-21 21:12:54 +0200

    add env variables and util package to deal handle it

---
+++ util/config.go
@@ -0,0 +1,24 @@
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	RabbitMqURL   string `mapstructure:"RABBIT_MQ"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.AddConfigPath("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
